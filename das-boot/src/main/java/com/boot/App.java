06c7b10b6890ca71b4d31f73c796a4acec89a813
Dadong Zhou <dadongz@yahoo.com>
2018-10-05 15:12:57 -0500

    checkin

--- das-boot/src/main/java/com/boot/App.java
+++ das-boot/src/main/java/com/boot/App.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
+	
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
